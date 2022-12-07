ec76524f38d9b7b9a31f30af3f32e2fef34a9cb1
Konstantin Azizov <azizovkostya97@gmail.com>
2022-12-07 07:25:28 +0100

    solution: 2022:07 refactor

--- 2022/07/index.js
+++ 2022/07/index.js
@@ -1,31 +1,9 @@
-const _ = require("lodash");
-
-function flattenObject(ob) {
-  var toReturn = {};
-
-  for (var i in ob) {
-    if (!ob.hasOwnProperty(i)) continue;
-
-    if (typeof ob[i] == "object" && ob[i] !== null) {
-      var flatObject = flattenObject(ob[i]);
-      for (var x in flatObject) {
-        if (!flatObject.hasOwnProperty(x)) continue;
-
-        toReturn[i + "." + x] = flatObject[x];
-      }
-    } else {
-      toReturn[i] = ob[i];
-    }
-  }
-  return toReturn;
-}
-
 module.exports = class Solution {
   /**
    * @param {string} input
    */
   constructor(input) {
-    this.fileTree = input.split("\n").reduce(
+    this.paths = input.split("\n").reduce(
       (state, line) => {
         if (line.startsWith("$ ")) {
           if (line.startsWith("$ cd")) {
@@ -45,66 +23,54 @@ module.exports = class Solution {
                 currentPath: [...state.currentPath, directory],
               };
             }
-          } else {
-            // Ignore `ls` command
-            return state;
           }
         } else {
-          const [size, filename] = line.split(" ");
+          const [size] = line.split(" ");
           const parsedSize = parseInt(size, 10);
 
-          if (Number.isNaN(parsedSize)) {
-            // Directory
+          if (Number.isNaN(parsedSize) === false) {
+            const newPaths = state.currentPath.reduce(
+              (paths, _directory, index) => {
+                const path = state.currentPath.slice(0, index + 1).join(".");
+                return {
+                  ...paths,
+                  [path]: (paths[path] || 0) + parsedSize,
+                };
+              },
+              state.paths
+            );
+
             return {
               ...state,
-              tree: _.set(state.tree, state.currentPath, {
-                ...(_.get(state.tree, state.currentPath) || {}),
-                [filename]: {
-                  sum: 0,
-                },
-              }),
+              paths: newPaths,
             };
-          } else {
-            for (let index = 1; index <= state.currentPath.length; index++) {
-              const currentValue = _.get(
-                state.tree,
-                state.currentPath.slice(0, index)
-              );
-
-              _.set(state.tree, state.currentPath.slice(0, index), {
-                ...currentValue,
-                sum: (currentValue?.sum || 0) + parsedSize,
-              });
-            }
-
-            return state;
           }
         }
+
+        return state;
       },
       {
         currentPath: [],
-        tree: {},
+        paths: {},
       }
-    ).tree;
+    ).paths;
   }
 
   partOne() {
-    return Object.entries(flattenObject(this.fileTree))
-      .filter(([key, value]) => key.endsWith(".sum") && value < 100000)
-      .map(([key, value]) => value)
+    const maxSize = 100_000;
+
+    return Object.values(this.paths)
+      .filter((value) => value < maxSize)
       .reduce((acc = 0, value) => acc + value);
   }
 
   partTwo() {
-    const totalSpace = 70000000;
-    const targetFreeSpace = 30000000;
-    const currentSize = this.fileTree["/"].sum;
+    const totalSpace = 70_000_000;
+    const targetFreeSpace = 30_000_000;
+    const currentSize = this.paths["/"];
     const neededSpace = totalSpace - targetFreeSpace;
-    const flattened = Object.entries(flattenObject(this.fileTree));
 
-    return flattened
-      .filter(([key]) => key.endsWith(".sum"))
-      .map(([, value]) => value)
+    return Object.values(this.paths)
       .sort((a, b) => a - b)
       .find((size) => currentSize - size <= neededSpace);
   }
